fa3bbf995759cee160f156700081158de6d5db71
firassios <firasgargouri@hotmail.fr>
2021-02-15 18:37:05 +0100

    Retrieve Products, Categories and SubCategories

--- product/urls.py
+++ product/urls.py
@@ -1,19 +1,24 @@
 from django.urls import path, include
-from .views import (ProductList, ProductCreate, CategoryList, SubCategoryList,)
+from .views import (ProductList, ProductCreate, ProductDetails, CategoryList, CategoryCreate,
+                    CategoryDetails, SubCategoryList, SubCategoryCreate, SubCategoryDetails,)
 
 app_name = 'product'
 
 urlpatterns = [
     # products
     path('all/', ProductList.as_view(), name='ProductList'),
-    path('create/', ProductCreate.as_view(), name='create'),
+    path('create/', ProductCreate.as_view(), name='ProductCreate'),
+    path('<int:pk>', ProductDetails.as_view(), name='ProductDetails'),
 
     # Categories
-    path('categories/all/', CategoryList.as_view(), name='CategoryList'),
-    path('create/', ProductCreate.as_view(), name='create'),
+    path('category/all/', CategoryList.as_view(), name='CategoryList'),
+    path('category/create/', CategoryCreate.as_view(), name='CategoryCreate'),
+    path('category/<int:pk>', CategoryDetails.as_view(), name='CategoryDetails'),
 
     # SubCategories
-    path('subCategories/all/', SubCategoryList.as_view(), name='SubCategoryList'),
-    path('create/', ProductCreate.as_view(), name='create'),
-    # path('<int:pk>', ProductCreate.as_view(), name='create'),
+    path('subcategory/all/', SubCategoryList.as_view(), name='SubCategoryList'),
+    path('subcategory/create/', SubCategoryCreate.as_view(),
+         name='SubCategoryCreate'),
+    path('subcategory/<int:pk>', SubCategoryDetails.as_view(),
+         name='SubCategoryDetails'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Product, Category, SubCategory
 from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
 
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -27,6 +27,7 @@ class SubCategoryList(generics.ListAPIView):
 
 
 class ProductCreate(APIView):
+    permission_classes = [permissions.IsAuthenticated, ]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, format=None):
@@ -38,6 +39,7 @@ class ProductCreate(APIView):
 
 
 class CategoryCreate(APIView):
+    permission_classes = [permissions.IsAuthenticated, ]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, format=None):
@@ -49,6 +51,7 @@ class CategoryCreate(APIView):
 
 
 class SubCategoryCreate(APIView):
+    permission_classes = [permissions.IsAuthenticated, ]
     parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, format=None):
@@ -57,3 +60,21 @@ class SubCategoryCreate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubCategoryDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
